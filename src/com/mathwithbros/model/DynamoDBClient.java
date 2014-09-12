7e64806daf554e1ae9b2036ddf49676852a56ba2
unknown <bryant.pham@BPHAM81HOU.pariveda.local>
2014-09-12 16:01:30 -0500

    Removed AWS access keys

--- src/com/mathwithbros/model/DynamoDBClient.java
+++ src/com/mathwithbros/model/DynamoDBClient.java
@@ -9,8 +9,8 @@ import com.amazonaws.auth.AWSCredentials;
 
 public class DynamoDBClient {
 
-	private final String accessKeyID = "AKIAIM77WHZK7SNTL73A";
-	private final String secretAccessKey = "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";
+	private final String accessKeyID = "SOME KEY";
+	private final String secretAccessKey = "SOME KEY";
 	
 	AWSCredentials credentials;
 	
